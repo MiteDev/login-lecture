2a76d965166b64dbeb2eb1a4f7719d35c750bfee
Dong <ssuny303@gmail.com>
2021-07-25 18:27:18 +0900

    css파일 삭제 & ejs 파일 비활성화

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@
 //모듈
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv"); //dotenv를 사용하면 어떤 os에서도 동일하게 환경변수 설정 가능
+const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = 3000;
@@ -17,11 +17,11 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
-//URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+
 app.use(express.urlencoded({extended: true}));
 
 
-app.use("/", home); // use -> 미들웨어를 등록해주는 메서드.
+app.use("/", home);
 
 
 module.exports = app;
